c4d6033523ea7602384efefe080398153768248a
Luke <sharlock@gmail.com>
2016-05-02 20:29:08 +1000

    refactor code

--- src/main/java/trainer/PowerBallTrainer.java
+++ src/main/java/trainer/PowerBallTrainer.java
@@ -33,44 +33,15 @@ public class PowerBallTrainer
 
         LogUtil.consoleLog("PowerBall Result Sample Size: " + results.size());
 
-//        calculatePowerBallBenchMark(results);
+        calculatePowerBallBenchMark(results);
 
-        IntStream.range(1,10).forEach(i -> calculatePowerHitBenchMark(results));
+        trainPowerHit(results, "PowerHit Benchmark", num -> true);
 
-//        calculatePowerHitNumberSimpleRule(results);
-
-
-
+        IntStream.range(1, PowerBallDraw.MAX_NUM).forEach(i -> trainPowerHit(results, "PowerHit ("+i+")", num -> num==i));
 
     }
 
-    private void calculatePowerHitNumberSimpleRule(List<PowerBallDraw> results)
-    {
-        for (int j = 1; j <= PowerBallDraw.MAX_NUM; j++)
-        {
-            List<TrainingResult> trainingResults = new ArrayList<>();
-            int expectedNum=j;
-            for (int k = 0; k < TRAINING_REPEAT_SIZE; k++)
-            {
-                TrainingResult trainingResult = new TrainingResult();
-
-                for (int i = 0; i < TRAINING_SIZE; i++)
-                {
-                    trainingOneSetResultForPowerHit(results, trainingResult, num -> num==expectedNum);
-                }
-                trainingResults.add(trainingResult);
-                System.out.print(".");
-            }
-            LogUtil.consoleLog("");
-
-            TrainingResult finalResult = getAverageTrainingResult(trainingResults);
-
-            LogUtil.consoleLog("PowerHit Benchmark("+expectedNum+"):" + finalResult.toString());
-
-        }
-    }
-
-    private void calculatePowerHitBenchMark(List<PowerBallDraw> results)
+    private void trainPowerHit(List<PowerBallDraw> results, String message, Predicate<Integer> predicate)
     {
         List<TrainingResult> trainingResults = new ArrayList<>();
         for (int k = 0; k < TRAINING_REPEAT_SIZE; k++)
@@ -79,7 +50,7 @@ public class PowerBallTrainer
 
             for (int i = 0; i < TRAINING_SIZE; i++)
             {
-                trainingOneSetResultForPowerHit(results, trainingResult, j -> true);
+                trainingOneSetResultForPowerHit(results, trainingResult, predicate);
             }
             trainingResults.add(trainingResult);
             System.out.print(".");
@@ -88,7 +59,7 @@ public class PowerBallTrainer
 
         TrainingResult finalResult = getAverageTrainingResult(trainingResults);
 
-        LogUtil.consoleLog("PowerHit Benchmark:" + finalResult.toString());
+        LogUtil.consoleLog(message + ":" + finalResult.toString());
     }
 
     private void calculatePowerBallBenchMark(List<PowerBallDraw> results)
@@ -115,21 +86,14 @@ public class PowerBallTrainer
     private TrainingResult getAverageTrainingResult(List<TrainingResult> trainingResults)
     {
         getRideAbnormalResults(trainingResults);
-        Long totalWin = 0L;
-        Long totalDivision = 0L;
-        Long totalTrainingSize = 0L;
-
-        for (TrainingResult trainingResult : trainingResults)
-        {
-            totalWin += trainingResult.getTotalWin();
-            totalDivision += trainingResult.getTotalDivision();
-            totalTrainingSize += trainingResult.getTotalTrainingSize();
-        }
+        Long totalWin = (long)trainingResults.stream().mapToLong(TrainingResult::getTotalWin).average().getAsDouble();
+        Long totalDivision = (long)trainingResults.stream().mapToLong(TrainingResult::getTotalDivision).average().getAsDouble();
+        Long totalTrainingSize = (long)trainingResults.stream().mapToLong(TrainingResult::getTotalTrainingSize).average().getAsDouble();
 
         TrainingResult averageTrainingResult = new TrainingResult();
-        averageTrainingResult.setTotalWin(totalWin / trainingResults.size());
-        averageTrainingResult.setTotalDivision(totalDivision / trainingResults.size());
-        averageTrainingResult.setTotalTrainingSize(totalTrainingSize / trainingResults.size());
+        averageTrainingResult.setTotalWin(totalWin);
+        averageTrainingResult.setTotalDivision(totalDivision);
+        averageTrainingResult.setTotalTrainingSize(totalTrainingSize);
 
         return averageTrainingResult;
     }
@@ -138,7 +102,7 @@ public class PowerBallTrainer
     {
         Collections.sort(trainingResults, (r1, r2) -> r1.getTotalDivision().compareTo(r2.getTotalDivision()));
         //temporary just remove first and last three elements
-        IntStream.range(1,10).forEach(i -> {
+        IntStream.range(1, 3).forEach(i -> {
             trainingResults.remove(0);
             trainingResults.remove(trainingResults.size() - 1);
         });
@@ -147,11 +111,11 @@ public class PowerBallTrainer
     private void trainingOneSetResult(List<PowerBallDraw> results, TrainingResult trainingResult, Predicate<Integer> predicate)
     {
         results.stream().forEach(r ->
-            //this is to make even with the power hit
-            IntStream.range(1,20).forEach(i -> {
-                int division = r.checkWin(PowerBallDraw.generateDraw(predicate));
-                trainingResult.addResult(division);
-            })
+                //this is to make even with the power hit
+                IntStream.range(1, 20).forEach(i -> {
+                    int division = r.checkWin(PowerBallDraw.generateDraw(predicate));
+                    trainingResult.addResult(division);
+                })
         );
     }
 
---
+++ src/test/java/trainer/PowerBallTrainerTest.java
@@ -0,0 +1,99 @@
+package trainer;
+
+import domain.draw.result.TrainingResult;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Luke on 2/05/2016.
+ */
+public class PowerBallTrainerTest
+{
+    PowerBallTrainer trainer = new PowerBallTrainer(new File(""));
+
+    TrainingResult trainingResult1 = new TrainingResult();
+    TrainingResult trainingResult2 = new TrainingResult();
+    TrainingResult trainingResult3 = new TrainingResult();
+    TrainingResult trainingResult4 = new TrainingResult();
+    TrainingResult trainingResult5 = new TrainingResult();
+    TrainingResult trainingResult6 = new TrainingResult();
+    TrainingResult trainingResult7 = new TrainingResult();
+    TrainingResult trainingResult8 = new TrainingResult();
+    TrainingResult trainingResult9 = new TrainingResult();
+    TrainingResult trainingResult10 = new TrainingResult();
+
+    List<TrainingResult> trainingResults = new ArrayList<>();
+
+
+    public PowerBallTrainerTest() throws URISyntaxException
+    {
+    }
+
+    @Before
+    public void setUp() throws Exception
+    {
+        trainingResult1.setTotalWin(11L);
+        trainingResult2.setTotalWin(22L);
+        trainingResult3.setTotalWin(33L);
+        trainingResult4.setTotalWin(44L);
+        trainingResult5.setTotalWin(55L);
+        trainingResult6.setTotalWin(66L);
+        trainingResult7.setTotalWin(77L);
+        trainingResult8.setTotalWin(88L);
+        trainingResult9.setTotalWin(99L);
+        trainingResult10.setTotalWin(101L);
+
+        trainingResult1.setTotalDivision(111l);
+        trainingResult2.setTotalDivision(222l);
+        trainingResult3.setTotalDivision(333l);
+        trainingResult4.setTotalDivision(444l);
+        trainingResult5.setTotalDivision(555l);
+        trainingResult6.setTotalDivision(666l);
+        trainingResult7.setTotalDivision(777L);
+        trainingResult8.setTotalDivision(888l);
+        trainingResult9.setTotalDivision(999l);
+        trainingResult10.setTotalDivision(1010l);
+
+        trainingResult1.setTotalTrainingSize(100L);
+        trainingResult2.setTotalTrainingSize(100L);
+        trainingResult3.setTotalTrainingSize(100L);
+        trainingResult4.setTotalTrainingSize(100L);
+        trainingResult5.setTotalTrainingSize(100L);
+        trainingResult6.setTotalTrainingSize(100L);
+        trainingResult7.setTotalTrainingSize(100L);
+        trainingResult8.setTotalTrainingSize(100L);
+        trainingResult9.setTotalTrainingSize(100L);
+        trainingResult10.setTotalTrainingSize(100L);
+
+        trainingResults.add(trainingResult1);
+        trainingResults.add(trainingResult2);
+        trainingResults.add(trainingResult3);
+        trainingResults.add(trainingResult4);
+        trainingResults.add(trainingResult5);
+        trainingResults.add(trainingResult6);
+        trainingResults.add(trainingResult7);
+        trainingResults.add(trainingResult8);
+        trainingResults.add(trainingResult9);
+        trainingResults.add(trainingResult10);
+
+    }
+
+    @Test
+    public void getAverageTrainingResult() throws Exception
+    {
+        Method method = trainer.getClass().getDeclaredMethod("getAverageTrainingResult", List.class);
+        method.setAccessible(true);
+        TrainingResult result = (TrainingResult) method.invoke(trainer, trainingResults);
+
+        System.out.println(result);
+
+    }
+}
