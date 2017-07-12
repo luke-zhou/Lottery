d35bab8da87b3e7beec2699cef38228bb213c395
Luke Zhou <lukezhou@Lukes-iMac.local>
2017-07-12 22:16:09 +1000

    Formating

--- src/main/java/trainer/OZLottoTrainer.java
+++ src/main/java/trainer/OZLottoTrainer.java
@@ -7,6 +7,7 @@ import domain.draw.OZLottoDraw;
 import domain.result.SeparateNumTrainingResult;
 import domain.result.TrainingResult;
 import util.LogUtil;
+import util.NumberGenUtil;
 
 import java.io.File;
 import java.util.Map;
@@ -20,12 +21,10 @@ import java.util.stream.IntStream;
  * Date: 24/10/2016
  * Time: 10:07 AM
  */
-public class OZLottoTrainer extends AbstractTrainer<OZLottoDraw>
-{
+public class OZLottoTrainer extends AbstractTrainer<OZLottoDraw> {
     private Map<Integer, OZLottoAnalyserResult> analyseResultMap;
 
-    public OZLottoTrainer(File resultFile)
-    {
+    public OZLottoTrainer(File resultFile) {
         super(resultFile, strings ->
         {
             Integer[] nums = IntStream.range(2, 9).mapToObj(i -> Integer.valueOf(strings[i])).toArray(size -> new Integer[size]);
@@ -39,35 +38,30 @@ public class OZLottoTrainer extends AbstractTrainer<OZLottoDraw>
     }
 
     @Override
-    public Map<Integer, ? extends AbstractAnalyserResult> getAnalyseResultMap()
-    {
+    public Map<Integer, ? extends AbstractAnalyserResult> getAnalyseResultMap() {
         return analyseResultMap;
     }
 
     @Override
-    public Consumer<TrainingResult> trainingBenchMark()
-    {
+    public Consumer<TrainingResult> trainingBenchMark() {
         return generateTrainingResultConsumer(
                 trainingResult -> generateOZLottoDrawConsumer(trainingResult,
                         draw -> OZLottoDraw.generateRandomDraw()
                 ));
     }
 
-    private Consumer<OZLottoDraw> generateOZLottoDrawConsumer(TrainingResult trainingResult, Function<OZLottoDraw, OZLottoDraw> generationFunction)
-    {
+    private Consumer<OZLottoDraw> generateOZLottoDrawConsumer(TrainingResult trainingResult, Function<OZLottoDraw, OZLottoDraw> generationFunction) {
         return r -> {
             OZLottoDraw generatedDraw = generationFunction.apply(r);
             int division = r.checkWin(generatedDraw);
             trainingResult.addResult(division);
-            if (division > 0 && printOutResultOn)
-            {
+            if (division > 0 && printOutResultOn) {
                 LogUtil.log(generatedDraw.toWinResultString(r));
             }
         };
     }
 
-    public Consumer<TrainingResult> trainingFrequency()
-    {
+    public Consumer<TrainingResult> trainingFrequency() {
         return generateTrainingResultConsumer(
                 trainingResult -> generateOZLottoDrawConsumer(trainingResult,
                         draw -> OZLottoDraw.generateDrawFrequencyByGroup(analyseResultMap.get(draw.getId() - 1))
@@ -75,37 +69,38 @@ public class OZLottoTrainer extends AbstractTrainer<OZLottoDraw>
     }
 
     //(ax+b)%45+1
-    public void trainSeparateNum(){
-        int aRange =100;
-        int bRange= 100;
-        int distanceRange =100;
+    public void trainSeparateNum() {
+        int aRange = 100;
+        int bRange = 100;
+        int distanceRange = 100;
 //        int numIndex =1;
-        for (int numIndex =1; numIndex < OZLottoDraw.NUM_OF_BALL; numIndex++)
-        for (int distance = 1; distance<=distanceRange; distance++) {
-            for (int a = -aRange; a <= aRange; a++) {
-                for (int b = -bRange; b <= bRange; b++) {
-                    SeparateNumTrainingResult result = new SeparateNumTrainingResult();
-                    result.setA(a);
-                    result.setB(b);
-                    result.setDistance(distance);
-                    result.setNumIndex(numIndex);
-                    for (int i = 0 + distance; i < results.size(); i++) {
-                        try {
-                            int expectNum = Math.abs((results.get(i - distance).getNum(numIndex) * a + b) % OZLottoDraw.MAX_NUM) + 1;
-                            int actualNum = results.get(i).getNum(numIndex);
-                            result.accumulateSize();
-                            result.accumulateMatch(expectNum == actualNum);
-                            result.accumulateDiff(Math.abs(expectNum - actualNum));
+        for (int numIndex = 1; numIndex < 2; numIndex++)
+            for (int distance = 1; distance <= distanceRange; distance++) {
+                for (int a = -aRange; a <= aRange; a++) {
+                    for (int b = -bRange; b <= bRange; b++) {
+                        SeparateNumTrainingResult result = new SeparateNumTrainingResult();
+                        result.setA(a);
+                        result.setB(b);
+                        result.setDistance(distance);
+                        result.setNumIndex(numIndex);
+                        for (int i = 0 + distance; i < results.size(); i++) {
+                            try {
+//                                int expectNum = Math.abs((results.get(i - distance).getNum(numIndex) * a + b) % OZLottoDraw.MAX_NUM) + 1;
+                                int expectNum = NumberGenUtil.randomGenerateNumber(45);
+                                int actualNum = results.get(i).getNum(numIndex);
+                                result.accumulateSize();
+                                result.accumulateMatch(expectNum == actualNum);
+                                result.accumulateDiff(Math.abs(expectNum - actualNum));
 
-                        } catch (Exception e) {
-                            e.printStackTrace();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
                         }
+                        if (result.getTotalMatch() > result.getTotalSize() / 16) System.out.println(result);
+                        if (result.getTotalDiff() < result.getTotalSize() *8) System.out.println(result);
                     }
-                    if (result.getTotalMatch() > result.getTotalSize() / 19) System.out.println(result);
-                    if (result.getTotalDiff() < result.getTotalSize() * 9) System.out.println(result);
                 }
             }
-        }
 
     }
 }
